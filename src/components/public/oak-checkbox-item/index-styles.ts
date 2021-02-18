6871b103a840b336b540896b55416bce96e0bb20
Arun Kumar Selvaraj <arunkumar.selvaraj@westernacher.com>
2021-02-18 23:47:33 +0530

    Implementation of Checkbox component

---
+++ src/components/public/oak-checkbox-item/index-styles.ts
@@ -0,0 +1,137 @@
+import {css} from 'lit-element';
+
+export const oakCheckboxItemStyles = css`
+  .oak-checkbox-item {
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    cursor: pointer;
+    flex: 0 0 auto;
+    overflow: visible;
+    text-align: center;
+    font-size: 1.5rem;
+    transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    border-radius: 50%;
+    border: 0;
+    margin: 0;
+    outline: 0;
+    /* padding: 0; */
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+    vertical-align: middle;
+    white-space: nowrap;
+    justify-content: center;
+    text-decoration: none;
+  }
+
+  .oak-checkbox-item__label-container {
+    user-select: auto;
+    -webkit-user-select: auto;
+    -moz-user-select: auto;
+    -ms-user-select: auto;
+    font-size: 14px;
+    white-space: nowrap;
+  }
+
+  .oak-checkbox-item__input-container {
+    width: 100%;
+    display: flex;
+    align-items: inherit;
+    justify-content: inherit;
+  }
+
+  .oak-checkbox-item__input {
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    opacity: 0;
+    padding: 0;
+    position: absolute;
+  }
+
+  .oak-checkbox-item__checkbox-svg {
+    width: 24px;
+    height: auto;
+  }
+
+  .oak-checkbox-item__checkbox-svg {
+    /* border: 1px solid transparent; */
+    border-radius: 2px;
+  }
+  /* .oak-checkbox-item__input:focus + .oak-checkbox-item__checkbox-svg {
+    border: 1px solid var(--formelement-outline-border-color);
+  } */
+
+  .oak-checkbox-item__checkbox-svg--notchecked {
+    fill: var(--color-1);
+  }
+
+  .oak-checkbox-item__checkbox-svg--checked.oak-checkbox-item__checkbox-svg--color-primary {
+    fill: var(--color-primary);
+  }
+  .oak-checkbox-item__checkbox-svg--checked.oak-checkbox-item__checkbox-svg--color-secondary {
+    fill: var(--color-secondary);
+  }
+  .oak-checkbox-item__checkbox-svg--checked.oak-checkbox-item__checkbox-svg--color-tertiary {
+    fill: var(--color-tertiary);
+  }
+  .oak-checkbox-item__checkbox-svg--checked.oak-checkbox-item__checkbox-svg--color-default {
+    fill: var(--color-default);
+  }
+  .oak-checkbox-item__checkbox-svg--checked.oak-checkbox-item__checkbox-svg--color-info {
+    fill: var(--color-info);
+  }
+  .oak-checkbox-item__checkbox-svg--checked.oak-checkbox-item__checkbox-svg--color-danger {
+    fill: var(--color-danger);
+  }
+  .oak-checkbox-item__checkbox-svg--checked.oak-checkbox-item__checkbox-svg--color-warning {
+    fill: var(--color-warning);
+  }
+  .oak-checkbox-item__checkbox-svg--checked.oak-checkbox-item__checkbox-svg--color-success {
+    fill: var(--color-success);
+  }
+  .oak-checkbox-item__checkbox-svg--checked.oak-checkbox-item__checkbox-svg--color-invert {
+    fill: var(--color-invert);
+  }
+
+  .oak-checkbox-item__input:focus
+    + .oak-checkbox-item__checkbox-svg--color-primary {
+    background-color: var(--color-primary-semitransparent1);
+  }
+  .oak-checkbox-item__input:focus
+    + .oak-checkbox-item__checkbox-svg--color-secondary {
+    background-color: var(--color-secondary-semitransparent1);
+  }
+  .oak-checkbox-item__input:focus
+    + .oak-checkbox-item__checkbox-svg--color-tertiary {
+    background-color: var(--color-tertiary-semitransparent1);
+  }
+  .oak-checkbox-item__input:focus
+    + .oak-checkbox-item__checkbox-svg--color-default {
+    background-color: var(--color-default-semitransparent1);
+  }
+  .oak-checkbox-item__input:focus
+    + .oak-checkbox-item__checkbox-svg--color-info {
+    background-color: var(--color-info-semitransparent1);
+  }
+  .oak-checkbox-item__input:focus
+    + .oak-checkbox-item__checkbox-svg--color-invert {
+    background-color: var(--color-invert-semitransparent1);
+  }
+  .oak-checkbox-item__input:focus
+    + .oak-checkbox-item__checkbox-svg--color-danger {
+    background-color: var(--color-danger-semitransparent1);
+  }
+  .oak-checkbox-item__input:focus
+    + .oak-checkbox-item__checkbox-svg--color-warning {
+    background-color: var(--color-warning-semitransparent1);
+  }
+  .oak-checkbox-item__input:focus
+    + .oak-checkbox-item__checkbox-svg--color-success {
+    background-color: var(--color-success-semitransparent1);
+  }
+`;
---
+++ src/components/public/oak-checkbox-item/index.ts
@@ -0,0 +1,155 @@
+import {LitElement, html, customElement, property} from 'lit-element';
+import {classMap} from 'lit-html/directives/class-map';
+import {globalStyles} from '../../../global-styles';
+import {
+  INPUT_CHANGE_EVENT,
+  INPUT_INPUT_EVENT,
+} from '../../../types/InputEventTypes';
+
+import {oakCheckboxItemStyles} from './index-styles';
+
+let elementIdCounter = 0;
+
+/**
+ * Checkbox item component.
+ *
+ */
+const customElementName = 'oak-checkbox-item';
+@customElement(customElementName)
+export class OakCheckboxItem extends LitElement {
+  private elementId = `${customElementName}-${elementIdCounter++}`;
+
+  @property({type: Boolean})
+  value? = false;
+
+  @property({type: String})
+  name = '';
+
+  @property({type: String})
+  color?:
+    | 'primary'
+    | 'secondary'
+    | 'tertiary'
+    | 'default'
+    | 'danger'
+    | 'warning'
+    | 'success'
+    | 'info' = 'primary';
+
+  /**
+   * 	If true, the element will have a bottom margin.
+   */
+  @property({type: Boolean})
+  gutterBottom?: boolean = false;
+
+  constructor() {
+    super();
+  }
+
+  private _handleChange = (event: any) => {
+    this.propagateEvent(INPUT_CHANGE_EVENT, event);
+    this.propagateEvent(INPUT_INPUT_EVENT, event);
+  };
+
+  private propagateEvent = (eventName: string, event: any) => {
+    this.dispatchEvent(
+      new CustomEvent(eventName, {
+        bubbles: true,
+        composed: true,
+        detail: {
+          id: event.srcElement.id,
+          name: event.srcElement.name,
+          value: !this.value,
+        },
+      })
+    );
+  };
+
+  private getClassMap(
+    baseClass:
+      | 'base'
+      | 'input-container'
+      | 'input'
+      | 'checkbox-svg'
+      | 'label-container'
+      | 'label'
+  ): any {
+    switch (baseClass) {
+      case 'base':
+        return {
+          [customElementName]: true,
+          'oak-gutter-bottom': this.gutterBottom,
+        };
+      case 'input-container':
+        return {
+          [`${customElementName}__${baseClass}`]: true,
+        };
+      case 'checkbox-svg':
+        return {
+          [`${customElementName}__${baseClass}`]: true,
+          [`${customElementName}__${baseClass}--color-${this.color}`]: true,
+          [`${customElementName}__${baseClass}--checked`]: this.value,
+          [`${customElementName}__${baseClass}--notchecked`]: !this.value,
+        };
+      case 'input':
+        return {
+          [`${customElementName}__${baseClass}`]: true,
+          [`${customElementName}__${baseClass}--hidden`]: true,
+        };
+      case 'label-container':
+        return {
+          [`${customElementName}__${baseClass}`]: true,
+        };
+      case 'label':
+        return {
+          [`${customElementName}__${baseClass}`]: true,
+        };
+      default:
+        return {};
+    }
+  }
+
+  static get styles() {
+    return [...globalStyles, oakCheckboxItemStyles];
+  }
+
+  render() {
+    return html`
+      <div class=${classMap(this.getClassMap('base'))}>
+        <span class=${classMap(this.getClassMap('input-container'))}>
+          <input
+            class=${classMap(this.getClassMap('input'))}
+            type="checkbox"
+            ?checked=${this.value}
+            id=${this.elementId}
+            @change=${this._handleChange}
+          />
+          ${this.value
+            ? html`<svg
+                class=${classMap(this.getClassMap('checkbox-svg'))}
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  d="M19 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.11 0 2-.9 2-2V5c0-1.1-.89-2-2-2zm-9 14l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"
+                ></path>
+              </svg>`
+            : html` <svg
+                class=${classMap(this.getClassMap('checkbox-svg'))}
+                focusable="false"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"
+                ></path>
+              </svg>`}
+        </span>
+        <span class=${classMap(this.getClassMap('label-container'))}>
+          <slot></slot>
+        </span>
+      </div>
+    `;
+  }
+}
